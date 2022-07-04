a3ed89207718ab08c8d57f6df2a9b638fe128344
medafcase <medafcase@gmail.com>
2022-07-04 15:38:00 +0000

    cd

--- client/src/config/firebase-config.js
+++ client/src/config/firebase-config.js
@@ -1,16 +1,42 @@
-// adding your firebase config here
 
-// import firebase from 'firebase/app';
-// import 'firebase/analytics';
-// const firebaseConfig = {
-// 	apiKey: '',
-// 	authDomain: '',
-// 	projectId: 'hosting-73b2e',
-// 	storageBucket: ,
-// 	messagingSenderId: ,
-// 	appId: ,
-// 	measurementId: 'G-6XQF3Y59RT',
-// };
+
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+const firebaseConfig = {
+  apiKey: "AIzaSyAZcb600QecyLoJd4I5DoF9ahps3UcdzaE",
+  authDomain: "shegergebeta-cc841.firebaseapp.com",
+  projectId: "shegergebeta-cc841",
+  storageBucket: "shegergebeta-cc841.appspot.com",
+  messagingSenderId: "786154733717",
+  appId: "1:786154733717:web:26800144dbc069551a5b9c",
+  measurementId: "G-DD0TJ8Q2XJ"
+};
 // // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
+
+
+
+/*
+
+
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+const firebaseConfig = {
+  apiKey: "AIzaSyAZcb600QecyLoJd4I5DoF9ahps3UcdzaE",
+  authDomain: "shegergebeta-cc841.firebaseapp.com",
+  projectId: "shegergebeta-cc841",
+  storageBucket: "shegergebeta-cc841.appspot.com",
+  messagingSenderId: "786154733717",
+  appId: "1:786154733717:web:26800144dbc069551a5b9c",
+  measurementId: "G-DD0TJ8Q2XJ"
+};
+// // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
+
+
+*/
+
+
+
